f356c76c6ff559e37e1866b83a9a162aa6fc599b
vycius <k.vycius@gmail.com>
2021-01-16 11:50:15 +0200

    Prefetch swellings

--- core/models.py
+++ core/models.py
@@ -566,6 +566,11 @@ class Swelling(models.Model):
         return self.swelling
 
 
+class DailyHealthStatusQuerySet(models.QuerySet):
+    def prefetch_swellings(self) -> DailyHealthStatusQuerySet:
+        return self.prefetch_related('swellings')
+
+
 class DailyHealthStatus(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='+')
     date = models.DateField()
@@ -607,6 +612,8 @@ class DailyHealthStatus(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    objects = DailyHealthStatusQuerySet.as_manager()
+
     class Meta:
         constraints = [
             models.UniqueConstraint(fields=['user', 'date'], name='unique_user_date_daily_health_status')
@@ -626,23 +633,25 @@ class DailyHealthStatus(models.Model):
 
     @staticmethod
     def get_earliest_user_entry_date(user: AbstractBaseUser) -> Optional[datetime.date]:
-        return DailyHealthStatus.get_for_user(user).aggregate(min_date=Min('date'))['min_date']
+        return DailyHealthStatus.filter_for_user(user).aggregate(min_date=Min('date'))['min_date']
 
     @staticmethod
     def has_any_statuses(user: AbstractBaseUser) -> bool:
-        return DailyHealthStatus.get_for_user(user).exists()
+        return DailyHealthStatus.filter_for_user(user).exists()
 
     @staticmethod
-    def get_for_user(user: AbstractBaseUser):
+    def filter_for_user(user: AbstractBaseUser) -> DailyHealthStatusQuerySet:
         return DailyHealthStatus.objects.filter(user=user)
 
     @staticmethod
     def get_for_user_and_date(user: AbstractBaseUser, date: datetime.date) -> Optional[DailyHealthStatus]:
-        return DailyHealthStatus.objects.filter(user=user, date=date).first()
+        return DailyHealthStatus.filter_for_user(user).filter(date=date).first()
 
     @staticmethod
-    def get_between_dates_for_user(user: AbstractBaseUser, date_from: datetime.date, date_to: datetime.date):
-        return DailyHealthStatus.objects.filter(user=user, date__range=(date_from, date_to)).order_by('date')
+    def get_between_dates_for_user(user: AbstractBaseUser, date_from: datetime.date,
+                                   date_to: datetime.date) -> DailyHealthStatusQuerySet:
+        return DailyHealthStatus.filter_for_user(user).filter(
+            date__range=(date_from, date_to)).prefetch_swellings().order_by('date')
 
     def __str__(self):
         return f"{self.user} {self.date}"
