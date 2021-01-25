9c0fb5a30b94f255af1244ac47d0644b8693bc9a
vycius <k.vycius@gmail.com>
2021-01-25 17:40:35 +0200

    Diabetes years filter diabetics

--- core/tasks.py
+++ core/tasks.py
@@ -67,7 +67,9 @@ def sync_product_metrics():
         diabetes_type__in=(DiabetesType.Type1, DiabetesType.Type2)))
 
     _gauge_aggregated_user_profile_group_metric('chronic_kidney_disease_years', _sick_years_groups)
-    _gauge_aggregated_user_profile_group_metric('diabetes_years', _sick_years_groups)
+    _gauge_aggregated_user_profile_group_metric('diabetes_years', _sick_years_groups,
+                                                user_profile_queryset=UserProfile.objects.filter(
+                                                    diabetes_type__in=(DiabetesType.Type1, DiabetesType.Type2)))
     _gauge_aggregated_user_profile_group_metric(
         'age', _user_age_groups,
         user_profile_queryset=UserProfile.objects.annotate_with_age()
