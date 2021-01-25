47b56a049ed97597cf1f20bb2ff882143ac5922c
vycius <k.vycius@gmail.com>
2021-01-25 12:47:44 +0200

    Additional user profile statistics

--- core/tasks.py
+++ core/tasks.py
@@ -2,11 +2,12 @@ import logging
 from datetime import timedelta
 
 from celery import shared_task
+from django.db.models.aggregates import Count
 
 from core.models import Appetite, DailyHealthStatus, DailyIntakesReport, HistoricalUserProfile, Intake, Product, \
     ProductKind, \
     ShortnessOfBreath, SwellingDifficulty, User, \
-    WellFeeling
+    UserProfile, WellFeeling
 from core.utils import Datadog
 from django.utils import timezone
 
@@ -28,6 +29,28 @@ def sync_product_metrics():
                   user_with_statistics_and_profile_queryset.exclude(intakes_count=0).count())
     datadog.gauge('product.users.profiles_with_health_status',
                   user_with_statistics_and_profile_queryset.exclude(daily_health_statuses_count=0).count())
+
+    users_by_gender = UserProfile.objects.values('gender').annotate(total=Count('gender')).order_by('total')
+    for gender in users_by_gender:
+        datadog.gauge('product.users.profiles.gender', gender['total'], tags=[f'gender:{gender["gender"]}'])
+
+    users_by_chronic_kidney_disease_stage = UserProfile.objects.values('chronic_kidney_disease_stage').annotate(
+        total=Count('chronic_kidney_disease_stage')).order_by('total')
+    for by_disease_stage in users_by_chronic_kidney_disease_stage:
+        datadog.gauge('product.users.profiles.chronic_kidney_disease_stage', by_disease_stage['total'],
+                      tags=[f'chronic_kidney_disease_stage:{by_disease_stage["chronic_kidney_disease_stage"]}'])
+
+    users_by_dialysis_type = UserProfile.objects.values('dialysis_type').annotate(
+        total=Count('dialysis_type')).order_by('total')
+    for by_dialysis_type in users_by_dialysis_type:
+        datadog.gauge('product.users.profiles.dialysis_type', by_dialysis_type['total'],
+                      tags=[f'dialysis_type:{by_dialysis_type["dialysis_type"]}'])
+
+    users_by_diabetes_complications = UserProfile.objects.values('diabetes_complications').annotate(
+        total=Count('diabetes_complications')).order_by('total')
+    for by_diabetes_complications in users_by_diabetes_complications:
+        datadog.gauge('product.users.profiles.diabetes_complications', by_diabetes_complications['total'],
+                      tags=[f'diabetes_complications:{by_diabetes_complications["diabetes_complications"]}'])
 
     datadog.gauge('product.users.last_sign_in.24_hours',
                   user_with_statistics_and_profile_queryset.filter(last_login__gte=now - timedelta(days=1)).count())
