56569b6b9945efbb4983cef177034fd22e596e62
vycius <k.vycius@gmail.com>
2021-06-02 08:50:35 +0300

    Upload fixed csv

--- core/management/commands/populate_swiss_products.py
+++ core/management/commands/populate_swiss_products.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         r = requests.get(
-            "https://gist.githubusercontent.com/vycius/6cb2b1148efcaa2d6d48b91e1169770e/raw/f2abc201a842d4c2594116b6f11caf1b05044b74/swiss.csv")
+            "https://gist.githubusercontent.com/vycius/6cb2b1148efcaa2d6d48b91e1169770e/raw/605b74d909ec6f804ffe04391e223b179fabf6be/swiss.csv")
         r.encoding = 'utf-8'
         r.raise_for_status()
 
@@ -23,26 +23,22 @@ class Command(BaseCommand):
             density = float(item['Density']) if kind == ProductKind.Drink else None
             multiplicator = density or 1
 
-            try:
-                defaults = {
-                    'name': item['Name'],
-                    'name_en': item['name_en'],
-                    'synonyms': item['Synonyms'],
-                    'product_kind': kind,
-                    'density_g_ml': density,
-                    'region': ProductRegion.DE,
-                    'potassium_mg': round(float(item['Potassium (K) (mg)']) * multiplicator),
-                    'proteins_mg': round(float(item['Protein (g)']) * 1000.0 * multiplicator),
-                    'sodium_mg': round(float(item['Sodium (Na) (mg)']) * multiplicator),
-                    'liquids_g': round(float(item['Water (g)']) * multiplicator),
-                    'energy_kcal': round(float(item['Energy, kilocalories (kcal)']) * multiplicator),
-                    'phosphorus_mg': round(float(item['Phosphorus (P) (mg)']) * multiplicator),
-                    'carbohydrates_mg': round(float(item['Carbohydrates, available (g)']) * 1000.0 * multiplicator),
-                    'fat_mg': round(float(item['Fat, total (g)']) * 1000.0 * multiplicator),
-                }
-            except:
-                continue
-
+            defaults = {
+                'name': item['Name'],
+                'name_en': item['name_en'],
+                'synonyms': item['Synonyms'],
+                'product_kind': kind,
+                'density_g_ml': density,
+                'region': ProductRegion.DE,
+                'potassium_mg': round(float(item['Potassium (K) (mg)']) * multiplicator),
+                'proteins_mg': round(float(item['Protein (g)']) * 1000.0 * multiplicator),
+                'sodium_mg': round(float(item['Sodium (Na) (mg)']) * multiplicator),
+                'liquids_g': round(float(item['Water (g)']) * multiplicator),
+                'energy_kcal': round(float(item['Energy, kilocalories (kcal)']) * multiplicator),
+                'phosphorus_mg': round(float(item['Phosphorus (P) (mg)']) * multiplicator),
+                'carbohydrates_mg': round(float(item['Carbohydrates, available (g)']) * 1000.0 * multiplicator),
+                'fat_mg': round(float(item['Fat, total (g)']) * 1000.0 * multiplicator),
+            }
 
             Product.objects.update_or_create(
                 raw_id=item['ID'],
